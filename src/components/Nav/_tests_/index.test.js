eb829a4e803416a67efe93fdd04ecfec32490bee
agvan <agvandyke25@gmail.com>
2022-09-06 12:57:03 -0500

    created and tested baseline test, test PASS

--- src/components/Nav/_tests_/index.test.js
+++ src/components/Nav/_tests_/index.test.js
@@ -1,15 +1,17 @@
 // importing packages needed for component testing
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Nav from '..';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
 
 // calls the cleanup() function using the afterEach global function from Jest
 afterEach(cleanup);
 
 // describe function declares the component that will be tested in this particular test suite
-describe('Nav component', () => {
-    // baseline test
-
-    // snapshot test
-})
+describe("Nav component", () => {
+  // baseline test
+  it("renders", () => {
+    render(<Nav />);
+  });
+  // snapshot test
+});
