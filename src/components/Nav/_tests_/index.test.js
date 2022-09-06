37745e1760de284a57316db26f2acd1db931b142
agvan <agvandyke25@gmail.com>
2022-09-06 12:38:20 -0500

    imported packages needed to test the Nav component

---
+++ src/components/Nav/_tests_/index.test.js
@@ -0,0 +1,5 @@
+// importing packages needed for component testing
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
