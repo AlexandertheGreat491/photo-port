96e5a1ebf33146c43b0fd921baf8e41e5e78e74b
agvan <agvandyke25@gmail.com>
2022-09-06 12:39:36 -0500

    added the cleanup statement

--- src/components/Nav/_tests_/index.test.js
+++ src/components/Nav/_tests_/index.test.js
@@ -3,3 +3,6 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
+
+// calls the cleanup() function using the afterEach global function from Jest
+afterEach(cleanup);
