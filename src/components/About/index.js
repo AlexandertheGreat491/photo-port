fe138c9518207ddff08976b66fc1ed9133da0ac3
agvan <agvandyke25@gmail.com>
2022-09-06 11:31:45 -0500

    finished Nav component and fixed code

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,20 @@
-// imports react
-import React from "react";
-// adds the cover image
+import React from 'react'
 import coverImage from "../../assets/cover/cover-image.jpg";
 
-// creates the JSX for the About component
 function About() {
+
   return (
-    <section className="my-5">
+    <section className="my-5" >
       <h1 id="about">Who am I?</h1>
-      <img
-        src={coverImage}
-        className="my-2"
-        style={{ width: "100%" }}
-        alt="cover"
-      />
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+      </p>
+      </div>
     </section>
-  );
+  )
 }
 
-// exports the About() for the About component
-export default About;
+export default About
+
