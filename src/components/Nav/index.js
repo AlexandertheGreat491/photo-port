47fcb2b9d996bc968663bf45ecf416c8fd542772
agvan <agvandyke25@gmail.com>
2022-09-06 10:36:37 -0500

    created the Nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+// imports react
+import React from 'react';
+
+// creates the nav component on the page
+function Nav() {
+    return (
+        <header>
+        </header>
+    );
+}
+
+// exports the Nav component
+export default Nav;
