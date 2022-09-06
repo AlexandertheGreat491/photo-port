3705a4f15afc2ff2894b6a5a5d8cd19e33264413
agvan <agvandyke25@gmail.com>
2022-09-06 12:14:51 -0500

    called the cleanup function using the afterEach global function

--- src/components/About/_tests_/index.test.js
+++ src/components/About/_tests_/index.test.js
@@ -1,1 +1,10 @@
-import React from 'react';
+import React from "react";
+// Jest creates a simulated DOM & removes components from the DOM to prevent memory leaking
+import { render, cleanup } from "@testing-library/react";
+// offers custom matchers that are used to test DOM elements
+import '@testing-library/jest-dom/extend-expect';
+// imports the About component
+import About from '..';
+
+// calls the cleanup() function using the afterEach global function from Jest
+afterEach(cleanup);
