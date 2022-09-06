acdaef2636b5a5138993458aaba18e3afa12c2da
agvan <agvandyke25@gmail.com>
2022-09-06 13:44:27 -0500

    all 6 tests passed & two test suites created

--- src/components/Nav/_tests_/index.test.js
+++ src/components/Nav/_tests_/index.test.js
@@ -1,31 +1,37 @@
-// importing packages needed for component testing
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Nav from "..";
+// __tests__/Nav.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
 
-// calls the cleanup() function using the afterEach global function from Jest
 afterEach(cleanup);
 
-// describe function declares the component that will be tested in this particular test suite
-describe("Nav component", () => {
-  // baseline test
-  it("renders", () => {
+describe('Nav component', () => {
+  it('renders', () => {
     render(<Nav />);
   });
-  // snapshot test
-  it("matches snapshot", () => {
+
+  it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    // assert value comparison
+    
     expect(asFragment()).toMatchSnapshot();
   });
-});
+})
 
-// describe function declares that the links are visible
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+  const { getByLabelText } = render(<Nav />);
+
+  expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})  
+
 describe('links are visible', () => {
-    it('inserts text into the links', () => {
-        // Arrange
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav />);
 
-        // Assert
-    })
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
 })
